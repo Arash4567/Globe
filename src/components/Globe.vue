746c18c023ee4ad14be9ad72f160090cfef8e30e
OG'ABEK <shomoyxhacker007@mail.ru>
2023-03-10 19:04:02 +0500

    Advanced Globe

--- src/components/Globe.vue
+++ src/components/Globe.vue
@@ -1,23 +1,12 @@
 <script setup>
-import * as THREE from 'three';
+import * as THREE from "three";
+import ThreeGlobe from "three-globe";
 import vertexShader from '../shaders/vertex.glsl'
 import fragmentShader from '../shaders/fragment.glsl'
-import atomosphereVertexShader from '../shaders/atomosphereVertex.glsl'
-import atomosphereFragmentShader from '../shaders/atomosphereFragment.glsl'
+import countries from "../assets/data/globe-data-min.json";
+import { TrackballControls } from "three/examples/jsm/controls/TrackballControls";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 1000);
-
-const renderer = new THREE.WebGLRenderer({
-  antialias: true
-});
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
-
-const sphere = new THREE.Mesh(
-  new THREE.SphereGeometry(5, 50, 50),
+const Globe = new ThreeGlobe(
   new THREE.ShaderMaterial({
     vertexShader,
     fragmentShader,
@@ -28,56 +17,74 @@ const sphere = new THREE.Mesh(
     }
   })
 )
+  .showAtmosphere(true)
+  .hexPolygonsData(countries.features)
+  .hexPolygonResolution(3)
+  .showGraticules(false)
+  .hexPolygonMargin(0.1)
+  .hexPolygonColor((e) => {
+    if (["USA", "UZB", "RUS"].includes(e.properties.ISO_A3)) {
+      return "#f00";
+    } else return "#d4d4d4";
+  });
 
-const atomosphere = new THREE.Mesh(
-  new THREE.SphereGeometry(5, 50, 50),
-  new THREE.ShaderMaterial({
-    vertexShader: atomosphereVertexShader,
-    fragmentShader: atomosphereFragmentShader,
-    blending: THREE.AdditiveBlending,
-    side: THREE.BackSide
-  })
-)
+const globeMaterial = Globe.globeMaterial();
+globeMaterial.opacity = 1;
+globeMaterial.color = new THREE.Color(0x00f);
 
-atomosphere.scale.set(1.1, 1.1, 1.1)
+// Setup renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+document.body.appendChild(renderer.domElement);
 
-scene.add(atomosphere)
+// Setup scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x000);
+scene.add(Globe);
+scene.add(new THREE.AmbientLight(0xbcbcbc, 2));
+scene.add(new THREE.AmbientLight(0xbcbcbc, 2));
 
-const group = new THREE.Group()
-group.add(sphere)
-scene.add(group)
+// Setup camera
+const camera = new THREE.PerspectiveCamera();
+camera.aspect = window.innerWidth / window.innerHeight;
+camera.updateProjectionMatrix();
+camera.position.z = 300;
 
-camera.position.z = 15
+// Add camera controls
+const tbControls = new TrackballControls(camera, renderer.domElement);
+tbControls.minDistance = 300;
+tbControls.maxDistance = 300;
+tbControls.rotateSpeed = 3;
 
-window.addEventListener("mousemove", onMouseMove, false);
+const clock = new THREE.Clock();
 
-var lastMove = [window.innerWidth/2, window.innerHeight/2];
-
-function onMouseMove(e) {
-  e = e || window.event;
-
-  //calculate difference between current and last mouse position
-  const moveX = ( e.clientX - lastMove[0]);
-  const moveY = ( e.clientY - lastMove[1]);
-  //rotate the globe based on distance of mouse moves (x and y) 
-  group.rotation.y += ( moveX * .005);
-  group.rotation.x += ( moveY * .005);
-
-  //store new position in lastMove
-  lastMove[0] = e.clientX;
-  lastMove[1] = e.clientY;
-}
-
-// animation
+// Kick-off renderer
 function animate() {
-  requestAnimationFrame(animate)
+  // IIFE
+  const elapsedTime = clock.getElapsedTime();
+  Globe.rotation.y = elapsedTime / 10;
+  // Frame cycle
+  tbControls.update();
   renderer.render(scene, camera);
-  group.rotation.y += 0.00001
+  requestAnimationFrame(animate);
 }
 
-animate()
+animate();
 </script>
 
-<template></template>
+<template>
+  <h1>Hello World!</h1>
+</template>
 
-<style scoped></style>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+h1 {
+  position: absolute;
+}
+</style>
